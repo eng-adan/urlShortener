e8be7944368d6de461fe2fde10efa97f0d77dec8
eng.adan <eng.madansarfraz@gmail.com>
2022-11-25 11:59:05 -0800

    Feature UI implementation with tailwind, webpacker and stimulus js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import '@fortawesome/fontawesome-free/css/all'
+import 'controllers'
+
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
