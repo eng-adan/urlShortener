872267dc84b8e588d8f754c7b4a9d6b094f6427f
eng.adan <eng.madansarfraz@gmail.com>
2022-12-01 09:01:20 -0800

    Handle Decryption and added click count in backend

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,10 @@
 class Link < ApplicationRecord
+    validates :actual_url, :lookup_code, presence: true
+
+    def self.fetch_url(code)
+        link = find_by(lookup_code: code)
+        link.update(clicks: (link.clicks.to_i + 1))
+
+        link.actual_url
+    end
 end
